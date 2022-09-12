0510788714fa88db6e5730bcccdcf188adffabb5
rowan habashy <69283634+Rowan123506@users.noreply.github.com>
2022-09-12 03:06:58 +0200

    Make Changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
-import EventDetails from '../views/EventDetails.vue'
+import EventDetails from "../views/event/Details.vue";
+import EventEdit from "../views/event/Edit.vue";
+import EventRegister from "../views/event/Register.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,8 +21,20 @@ const router = createRouter({
     {
       path: "/event/:id",
       name: "EventDetails",
-      props:true,
       component: EventDetails,
+      props: true,
+    },
+    {
+      path: "/event/:id/edit",
+      name: "EventEdit",
+      component: EventEdit,
+      props: true,
+    },
+    {
+      path: "/event/:id/register",
+      name: "EventRegister",
+      component: EventRegister,
+      props: true,
     },
   ],
 });
---
+++ src/views/event/Details.vue
@@ -0,0 +1,64 @@
+<template>
+  <div v-if="event">
+    <h1 class="p-7">{{ event.title }}</h1>
+    <div class="nav-style" id="nav">
+      <router-link :to="{ name: 'EventDetails', params: { id } }"
+        >Details</router-link
+      >
+      |
+      <router-link :to="{ name: 'EventRegister', params: { id } }"
+        >Register</router-link
+      >
+      |
+      <router-link :to="{ name: 'EventEdit', params: { id } }"
+        >Edit</router-link
+      >
+    </div>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+  <div class="eventDetails" v-else>
+    <b-button variant="primary" disabled>
+      <b-spinner small></b-spinner>
+      <span class="sr-only">Loading...</span>
+    </b-button>
+  </div>
+</template>
+
+<script>
+import EventService from "@/services/EventService.js";
+export default {
+  props: ["id"],
+  data() {
+    return {
+      event: null,
+    };
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then((response) => {
+        this.event = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
+<style scoped>
+.p-7 {
+  padding-top: 71px;
+}
+.nav-style {
+  background-color: beige;
+  padding: 12px;
+}
+.eventDetails {
+  padding-top: 80px;
+}
+.sr-only {
+  font-size: 40px;
+  font-weight: 900;
+  color: #104471;
+}
+</style>
